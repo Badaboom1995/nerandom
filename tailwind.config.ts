04fed796d5ff1f0f4ee281f9a77ac0a687c90409
Aleksey Belov <badavoo@Alekseys-MacBook-Pro.local>
2022-08-22 20:21:40 +0800

    coffee layout

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+export {};
+
+module.exports = {
+  content: ["./src/**/*.{html,ts, tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
+    },
+  },
+  plugins: [],
+};
