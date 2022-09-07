bacdbb1825ae1cc382be8b5cc20fb0ddf5f99e04
badavoo <lordalurard@gmail.com>
2022-09-07 20:10:35 +0800

    debug

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Field } from "formik";
+import { Field, useField } from "formik";
 
 const Input = ({
   name,
@@ -18,6 +18,8 @@ const Input = ({
 }) => {
   const styles =
     " hover:outline-none focus:outline-none cursor-pointer p-2 m-1 bg-gray-50 rounded-lg border border-gray-200 inline-block box-border";
+  const [field, meta, helpers] = useField(name);
+
   return (
     <div className={`flex flex-col ${className}`}>
       <label className={`font-medium text-md pl-3 mb-2`}>{label}</label>
@@ -29,6 +31,7 @@ const Input = ({
           cols={12}
           className={styles}
           placeholder={placeholder}
+          value={meta?.value}
         />
       ) : (
         <Field
@@ -36,6 +39,7 @@ const Input = ({
           name={name}
           className={styles}
           placeholder={placeholder}
+          value={meta?.value}
         />
       )}
     </div>
